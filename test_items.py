dbc42cf9a3a48039f11d95b20b384b88cface0a2
Peter-Bud <Budzinskuy_petro@i.ua>
2020-07-23 00:16:40 +0300

    debug test_items

--- test_items.py
+++ test_items.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_add_to_basket_button(browser):
+def test_should_see_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(5)
     is_there=None
