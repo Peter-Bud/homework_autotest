3c58a2bce6037d2d6a2a011d8e1f49326b9c6ed6
Peter-Bud <Budzinskuy_petro@i.ua>
2020-07-22 23:54:25 +0300

    debug conftest

--- conftest.py
+++ conftest.py
@@ -12,6 +12,7 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language= request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         options = Options()
